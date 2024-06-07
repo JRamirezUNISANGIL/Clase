9eed3f4f94d31a253d0fd9a350ed6d8151b534dc
Julian <jramirez@unisangil.edu.co>
2024-06-07 09:02:16 -0500

    agregamos Pruebas.py

---
+++ Pruebas.py
@@ -0,0 +1,13 @@
+
+valores = []
+
+for i in range(0,10,1):
+    valor = float(input(f"Ingrese un valor {i+1}: "))
+    valores.append(valor)
+val= float(input())
+for j in range(0,10,1):
+    if(val == valores[j]):
+        print("hola")
+    
+
+
